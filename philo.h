306fc5e9fd6a2c11b4f2408c9dcee6ba4ae20b9f
Shotaro Mizuochi <smizuoch@c1r22s8.42tokyo.jp>
2023-11-25 15:32:01 +0900

    first commit

---
+++ philo.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smizuoch <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/25 13:55:38 by smizuoch          #+#    #+#             */
+/*   Updated: 2023/11/25 14:03:35 by smizuoch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include<string.h>
+# include<stdlib.h>
+# include<stdio.h>
+# include<unistd.h>
+# include<pthread.h>
+
+#endif
