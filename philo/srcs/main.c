94b430eff62dbe332448bdc44a4aa4fbf0ebfcd0
Shotaro Mizuochi <smizuoch@c1r16s8.42tokyo.jp>
2023-12-19 13:22:13 +0900

    add threads

--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: smizuoch <smizuoch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/25 13:48:14 by smizuoch          #+#    #+#             */
-/*   Updated: 2023/12/12 15:46:02 by smizuoch         ###   ########.fr       */
+/*   Updated: 2023/12/19 13:20:49 by smizuoch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,25 @@
 
 int	return_error(void)
 {
-	write(2, "error in argument\n", 19);
+	write(2, "error\n", 7);
 	return (1);
 }
 
 int	main(int argc, char **argv)
 {
-	t_arg	arg;
+	int			i;
+	t_arg		arg;
+	pthread_t	*threads;
 
+	i = 0;
 	arg.number_of_times_each_philosopher_must_eat = -1;
 	if (argc < 5 || 6 < argc)
 		return (return_error());
 	if (init_arg(argc, argv, &arg))
 		return (return_error());
-	printf("%d\n%d\n%d\n%d\n%d\n", arg.number_of_philosophers, arg.time_to_die, arg.time_to_eat, arg.time_to_sleep, arg.number_of_times_each_philosopher_must_eat);
+	threads = malloc(sizeof(pthread_t) * arg.number_of_philosophers);
+	if (!threads)
+		return (return_error());
+	free(threads);
+	return (0);
 }
