ba3a74254a4a26e0c0269c745e9670c855677996
Shotaro Mizuochi <smizuoch@c1r16s8.42tokyo.jp>
2023-12-26 14:56:11 +0900

    fix header

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: smizuoch <smizuoch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/25 13:55:38 by smizuoch          #+#    #+#             */
-/*   Updated: 2023/12/12 15:51:59 by smizuoch         ###   ########.fr       */
+/*   Updated: 2023/12/26 14:54:31 by smizuoch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,19 +20,32 @@
 # include <pthread.h>
 # include <limits.h>
 
-typedef struct s_arg
+typedef struct s_config t_config;
+
+typedef struct s_philo
 {
-	int	number_of_philosophers;
-	int	time_to_die;
-	int	time_to_eat;
-	int	time_to_sleep;
-	int	number_of_times_each_philosopher_must_eat;
-}t_arg;
+	int			id;
+	t_config	*config;
+	pthread_mutex_t	lock;
+	pthread_t	thread;
+}	t_philo;
+
+struct s_config
+{
+	pthread_mutex_t	*forks;
+	
+	int		number_of_philosophers;
+	int		time_to_die;
+	int		time_to_eat;
+	int		time_to_sleep;
+	int		number_of_times_each_philosopher_must_eat;
+	t_philo	*philos;
+};
 
 int	philo_atoi(const char *str);
 int	ft_isdigit(int c);
 int	str_isdigit(char *str);
-int	init_arg(int argc, char **argv, t_arg *arg);
+int	init_arg(int argc, char **argv, t_config *config);
 int	check_arg(int argc, char **argv);
 
 #endif
--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: smizuoch <smizuoch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/25 13:48:14 by smizuoch          #+#    #+#             */
-/*   Updated: 2023/12/19 13:20:49 by smizuoch         ###   ########.fr       */
+/*   Updated: 2023/12/26 13:38:45 by smizuoch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,16 +21,16 @@ int	return_error(void)
 int	main(int argc, char **argv)
 {
 	int			i;
-	t_arg		arg;
+	t_config	config;
 	pthread_t	*threads;
 
 	i = 0;
-	arg.number_of_times_each_philosopher_must_eat = -1;
+	config.number_of_times_each_philosopher_must_eat = -1;
 	if (argc < 5 || 6 < argc)
 		return (return_error());
-	if (init_arg(argc, argv, &arg))
+	if (init_arg(argc, argv, &config))
 		return (return_error());
-	threads = malloc(sizeof(pthread_t) * arg.number_of_philosophers);
+	threads = malloc(sizeof(pthread_t) * config.number_of_philosophers);
 	if (!threads)
 		return (return_error());
 	free(threads);
