d79e0238af9a4813d7680c64669d494720221b1f
Shotaro Mizuochi <smizuoch@c1r16s8.42tokyo.jp>
2023-12-29 08:29:07 +0900

    fix norm

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: smizuoch <smizuoch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/25 13:55:38 by smizuoch          #+#    #+#             */
-/*   Updated: 2023/12/27 17:12:29 by smizuoch         ###   ########.fr       */
+/*   Updated: 2023/12/29 08:28:45 by smizuoch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,34 +20,33 @@
 # include <pthread.h>
 # include <limits.h>
 
-typedef struct s_config t_config;
+typedef struct s_config	t_config;
 
 typedef struct s_philo
 {
-	int			id;
-	t_config	*config;
+	int				id;
+	t_config		*config;
 	pthread_mutex_t	lock;
-	pthread_t	thread;
+	pthread_t		thread;
 }	t_philo;
 
 struct s_config
 {
 	pthread_mutex_t	*forks;
-	
-	int		number_of_philosophers;
-	int		time_to_die;
-	int		time_to_eat;
-	int		time_to_sleep;
-	int		number_of_times_each_philosopher_must_eat;
-	t_philo	*philos;
+	int				number_of_philosophers;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				number_of_times_each_philosopher_must_eat;
+	t_philo			*philos;
 };
 
-int	philo_atoi(const char *str);
-int	ft_isdigit(int c);
-int	str_isdigit(char *str);
-int	init_arg(int argc, char **argv, t_config *config);
-int	check_arg(int argc, char **argv);
+int		philo_atoi(const char *str);
+int		ft_isdigit(int c);
+int		str_isdigit(char *str);
+int		init_arg(int argc, char **argv, t_config *config);
+int		check_arg(int argc, char **argv);
 t_philo	*init_philo(t_config *config);
-int	start_life(t_config *config, t_philo *philosophers);
+int		start_life(t_config *config, t_philo *philosophers);
 
 #endif
