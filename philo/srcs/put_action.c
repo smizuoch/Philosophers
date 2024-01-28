15584e62d1102bff5b2e764f536208562fe63912
smizuoch <shotaro.mizuochi@proton.me>
2024-01-28 14:20:32 +0900

    fix

--- philo/srcs/put_action.c
+++ philo/srcs/put_action.c
@@ -6,22 +6,15 @@
 /*   By: smizuoch <smizuoch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/29 11:18:18 by smizuoch          #+#    #+#             */
-/*   Updated: 2024/01/17 12:01:49 by smizuoch         ###   ########.fr       */
+/*   Updated: 2024/01/28 12:53:10 by smizuoch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
-void put_action(t_philo *philo, char *action)
+void	put_action(t_philo *philo, char *action)
 {
 	(void)philo;
 	(void)action;
 	return ;
-	// int	time;
-
-	// time = get_time();
-	// pthread_mutex_lock(&philo->config->write);
-	// if (philo->config->number_of_philosophers > 0)
-	// 	printf("%d %d %s\n", time, philo->id, action);
-	// pthread_mutex_unlock(&philo->config->write);
 }
