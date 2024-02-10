cd34921e95e676d651abdad8e1234e87c0b4df0b
smizuoch <shotaro.mizuochi@proton.me>
2024-02-10 11:47:41 +0900

    add strlen

--- philo/srcs/do_eat.c
+++ philo/srcs/do_eat.c
@@ -6,7 +6,7 @@
 /*   By: smizuoch <smizuoch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/29 11:28:35 by smizuoch          #+#    #+#             */
-/*   Updated: 2024/02/07 17:02:06 by smizuoch         ###   ########.fr       */
+/*   Updated: 2024/02/10 11:46:51 by smizuoch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ int	do_eat(t_philo *philo)
 		philo->config->number_of_philosophers--;
 	philo->last_meal_time = get_time();
 	pthread_mutex_unlock(&philo->config->forks[philo->id - 1]);
-	pthread_mutex_unlock(&philo->config->forks[(philo->id) % philo->config->number_of_philosophers]);
+	pthread_mutex_unlock(&philo->config->forks[(philo->id)
+		% philo->config->number_of_philosophers]);
 	return (0);
 }
--- philo/srcs/ft_isdigit.c
+++ philo/srcs/ft_isdigit.c
@@ -6,11 +6,23 @@
 /*   By: smizuoch <smizuoch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 16:43:25 by smizuoch          #+#    #+#             */
-/*   Updated: 2023/12/29 12:03:44 by smizuoch         ###   ########.fr       */
+/*   Updated: 2024/02/10 11:44:25 by smizuoch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	c;
+
+	c = 0;
+	while (s[c])
+	{
+		c ++;
+	}
+	return (c);
+}
 
 int	ft_isdigit(int c)
 {
@@ -41,6 +53,8 @@ int	check_arg(int argc, char **argv)
 	while (i < argc)
 	{
 		if (str_isdigit(argv[i]))
+			return (1);
+		if (ft_strlen(argv[i]) > 10)
 			return (1);
 		i++;
 	}
