8753bb2e59f11faa09020d7947c926810b19c059
smizuoch <shotaro.mizuochi@proton.me>
2024-02-14 13:38:52 +0900

    add time

--- philo/srcs/time.c
+++ philo/srcs/time.c
@@ -6,11 +6,32 @@
 /*   By: smizuoch <smizuoch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/29 11:47:16 by smizuoch          #+#    #+#             */
-/*   Updated: 2024/02/10 13:47:28 by smizuoch         ###   ########.fr       */
+/*   Updated: 2024/02/14 13:38:13 by smizuoch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
+
+time_t	usec_to_msec(time_t usec)
+{
+	return (usec / 1000);
+}
+
+int	put_message(t_philo *philo, char *action)
+{
+	time_t	time;
+
+	
+	pthread_mutex_lock(&philo->config->mutex);
+	time = get_time();
+	time = usec_to_msec(time);
+	if (philo->config->observer == 0)
+	{
+		printf("%ld %d %s\n", time, philo->id, action);
+	}
+	pthread_mutex_unlock(&philo->config->mutex);
+	return (0);
+}
 
 time_t	get_time(void)
 {
