6ccd773bf06a39e07dbeac0bb20d0e82b8b3e6c4
smizuoch <shotaro.mizuochi@proton.me>
2024-02-21 11:52:13 +0900

    add sleep & think

--- philo/srcs/do_sleep.c
+++ philo/srcs/do_sleep.c
@@ -6,8 +6,16 @@
 /*   By: smizuoch <smizuoch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/29 11:31:08 by smizuoch          #+#    #+#             */
-/*   Updated: 2024/02/20 08:50:15 by smizuoch         ###   ########.fr       */
+/*   Updated: 2024/02/21 11:49:44 by smizuoch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
+
+int	do_sleep(t_philo *philo)
+{
+	if (print_time_doing(philo, "is sleeping\n"))
+		return (1);
+	ft_usleep(philo->config->time_to_sleep);
+	return (0);
+}
--- philo/srcs/do_think.c
+++ philo/srcs/do_think.c
@@ -6,8 +6,15 @@
 /*   By: smizuoch <smizuoch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/29 11:28:33 by smizuoch          #+#    #+#             */
-/*   Updated: 2024/02/20 09:02:36 by smizuoch         ###   ########.fr       */
+/*   Updated: 2024/02/21 11:50:43 by smizuoch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
+
+int	do_think(t_philo *philo)
+{
+	if (print_time_doing(philo, "is thinking\n"))
+		return (1);
+	return (0);
+}
